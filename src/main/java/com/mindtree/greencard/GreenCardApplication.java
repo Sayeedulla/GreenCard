b09fc64df340944362facfe737c2ff1219364ef3
Dillan Fernando <m1047048@mindtree.com>
2018-08-31 12:11:08 +0000

    Update GreenCardApplication.java

--- src/main/java/com/mindtree/greencard/GreenCardApplication.java
+++ src/main/java/com/mindtree/greencard/GreenCardApplication.java
@@ -1,13 +1,16 @@
 package com.mindtree.greencard;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.mindtree.greencard")
 @SpringBootApplication
+@EnableJpaAuditing
 public class GreenCardApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
