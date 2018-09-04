de9aa78701af01e9cf52123f1e74ffb18ad0a50e
Tarun Chawla <m1046885@mindtree.com>
2018-09-04 04:21:02 +0000

    Update WebConfig.java

--- src/main/java/com/mindtree/greencard/WebConfig.java
+++ src/main/java/com/mindtree/greencard/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+		    @Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
