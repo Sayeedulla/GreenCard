14cfa1b5a38c614a056c3ff683b3ff39b0c47d58
Dillan Fernando <m1047048@mindtree.com>
2018-09-05 22:00:59 +0530

    pushing service side

--- src/main/java/com/mindtree/greencard/controller/SuperAdminController.java
+++ src/main/java/com/mindtree/greencard/controller/SuperAdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class SuperAdminController {
 	@Autowired
 	private SuperAdminService service;
 
-	@RequestMapping(value = "/addUser")
+	@PostMapping(value = "/addUser")
 	private String addUser(@RequestBody User user) {
 		return this.service.addUser(user);
 	}
