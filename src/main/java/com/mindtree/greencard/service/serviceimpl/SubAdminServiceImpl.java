973563f46742c9992bf083e9a6f42d60e7fde574
Praveenkumar Srinivasan <m1046874@mindtree.com>
2018-09-07 02:04:48 +0530

    update

--- src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
+++ src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
@@ -1,28 +1,46 @@
 package com.mindtree.greencard.service.serviceimpl;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mindtree.greencard.jprepository.adminrepository.GreenCardHistoryRepository;
 import com.mindtree.greencard.jprepository.adminrepository.InProgressGreenCardRepository;
+import com.mindtree.greencard.jprepository.greencardrepository.GreenCardLifeCycleRepository;
 import com.mindtree.greencard.jprepository.greencardrepository.NewGreenCardRepository;
+import com.mindtree.greencard.jprepository.superadminrepository.UserRepository;
+import com.mindtree.greencard.model.GreenCardHistory;
+import com.mindtree.greencard.model.GreenCardLifeCycle;
 import com.mindtree.greencard.model.InProgressGreenCard;
 import com.mindtree.greencard.model.NewGreenCard;
-import com.mindtree.greencard.jprepository.greencardrepository.GreenCardLifeCycleRepository;
-import com.mindtree.greencard.model.GreenCardLifeCycle;
+import com.mindtree.greencard.model.User;
 import com.mindtree.greencard.service.SubAdminService;
+
 @Service
-public class SubAdminServiceImpl implements SubAdminService{
+public class SubAdminServiceImpl implements SubAdminService {
 
 	@Autowired
 	InProgressGreenCardRepository inProgGCRepo;
 
 	@Autowired
 	NewGreenCardRepository newGCRepo;
-	
+
 	@Autowired
 	GreenCardLifeCycleRepository greencardLCRepo;
+
+	@Autowired
+	GreenCardHistoryRepository gcHR;
+
+	@Autowired
+	User u;
+	
+	@Autowired
+	GreenCardHistory gcH;
+	
+	@Autowired
+	UserRepository us;
 
 	@Override
 	public List<InProgressGreenCard> getComplaints(String mid) {
@@ -41,16 +59,37 @@ public class SubAdminServiceImpl implements SubAdminService{
 
 	@Override
 	public String updateComplaint(InProgressGreenCard sub) {
+		//int id;
 		if (inProgGCRepo.existsById(sub.getlId())) {
 			inProgGCRepo.save(sub);
 			GreenCardLifeCycle greencardLC = greencardLCRepo.getOne(sub.getlId());
-			greencardLC.setStatus("Solved");
+			NewGreenCard ngc = newGCRepo.getOne(sub.getGcId());
+			
+			greencardLC.setStatus("Closed");
+			greencardLC.setResolvedTime(LocalDateTime.now());
 			greencardLCRepo.save(greencardLC);
-			return "Passed for Review";
-		}
-		else {
+
+			gcH.setgId(sub.getGcId());
+			gcH.setAssignedPersonId(sub.getAssignedPersonId());
+			gcH.setCategory(sub.getCategory());
+			gcH.setClosedDateTime(greencardLC.getResolvedTime());
+			gcH.setCorrectiveAction(sub.getCorrectiveAction());
+			gcH.setImage(ngc.getImage());
+			gcH.setLandmark(ngc.getLandmark());
+			gcH.setRootCause(sub.getRootCause());
+			gcH.setStatus(greencardLC.getStatus());
+			gcH.setSubmittedDateTime(greencardLC.getAssignedTime());
+			//User u=this.us.findUserId(170);
+			gcH.setWhatHappened(ngc.getWhatHappened());
+			gcHR.save(gcH);
+			
+			greencardLCRepo.delete(greencardLC);
+			inProgGCRepo.delete(sub);
+			newGCRepo.delete(ngc);
+			
+			return "Comlaint Resolved";
+		} else {
 			return "Particular complaint not exist";
 		}
 	}
-
 }
