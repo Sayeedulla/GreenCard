65bd3e34f547807deb07e42d6647548736ec9da0
Praveenkumar Srinivasan <m1046874@mindtree.com>
2018-09-11 07:35:21 +0000

    Update NewGreenCard.java

--- src/main/java/com/mindtree/greencard/model/NewGreenCard.java
+++ src/main/java/com/mindtree/greencard/model/NewGreenCard.java
@@ -27,17 +27,6 @@ public class NewGreenCard {
 	@Lob
 	byte[] image;
 	
-	@ManyToOne
-	private User user;
-	
-	
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
 	public Integer getGreenCardId() {
 		return greenCardId;
 	}
