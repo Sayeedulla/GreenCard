67ef058e84d07d9ea4c202f083f71fd1db3ce96f
Sayeedulla Khansamiullakhan <m1046908@mindtree.com>
2018-09-17 14:40:22 +0530

    FinalChanges

--- src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
+++ src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
@@ -128,10 +128,11 @@ public class SubAdminServiceImpl implements SubAdminService {
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.port", "587");
 
 		Session session = Session.getDefaultInstance(props,
 			new javax.mail.Authenticator() {
@@ -145,7 +146,7 @@ public class SubAdminServiceImpl implements SubAdminService {
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("stng361@gmail.com"));
 			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("sayeed150696@gmail.com"));
+					InternetAddress.parse("Kusal.Bandaru@mindtree.com"));
 			message.setSubject(mid+" Required Help for GreenCard Id "+gc_id);
 			message.setText(desc);
 
