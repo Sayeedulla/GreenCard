acc93ec6f9fb91b14b39f586842a4f91f6128cf1
Sayeedulla Khansamiullakhan <m1046908@mindtree.com>
2018-09-19 15:14:40 +0530

    ExceptionsHandled

--- src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
+++ src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
@@ -126,13 +126,14 @@ public class SubAdminServiceImpl implements SubAdminService {
 	@Override
 	public String sendHelpEmail(String mid, int gc_id, String desc) {
 		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.host", "smtp-mail.outlook.com");
+		props.put("mail.smtp.socketFactory.port", "587");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.socketFactory.fallback", "true");
 
 		Session session = Session.getDefaultInstance(props,
 			new javax.mail.Authenticator() {
