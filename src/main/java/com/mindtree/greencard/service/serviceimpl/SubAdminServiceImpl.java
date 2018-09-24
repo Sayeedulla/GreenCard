ce83d39780f779cba354b4bd669f3423906a0d29
Sayeedulla Khansamiullakhan <m1046908@mindtree.com>
2018-09-24 06:21:48 +0000

    Update SubAdminServiceImpl.java

--- src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
+++ src/main/java/com/mindtree/greencard/service/serviceimpl/SubAdminServiceImpl.java
@@ -177,10 +177,14 @@ public class SubAdminServiceImpl implements SubAdminService {
 	public String sendHelpEmail(String mid, int gcId, String desc) throws ServiceException {
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.starttls.enable", "true");
+		
+		
+		
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
-		props.put("mail.smtp.socketFactory.fallback", "true");
+		
+		props.put("mail.smtp.socketFactory.fallback", "false");
+		props.put("mail.smtp.socketFactory.port", "465");
+		
 		
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
